18b005e8b210c1abb645b03b976573384a73638b
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-01-11 12:23:15 -0300

    :sparkles: feat: Envio de mensagem para o kafka

--- kafka-demo/src/main/java/com/xxnbr/App.java
+++ kafka-demo/src/main/java/com/xxnbr/App.java
@@ -1,13 +1,32 @@
 package com.xxnbr;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        var properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        var producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i <= 1000; i++) {
+            producer.send(new ProducerRecord<>("test-java", String.valueOf(i), "MSG = " + i));
+        }
+
+        producer.close();
+
     }
 }
